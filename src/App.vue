ae559849be8a8395efad579489d2f9d28e9a51bc
imehc <richohealthy@hotmail.com>
2024-05-17 14:28:35 +0800

    feat: upgrade dependencies

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@ onMounted(() => {
 
 <template>
   <div className="text-3xl font-bold underline text-orange-500 m-10">
-    <!-- https://github.com/unplugin/unplugin-icons#readme -->
-    <!-- https://icon-sets.iconify.design/emojione-v1/panda-face/ -->
     <i-emojione-v1-panda-face />
   </div>
 </template>
