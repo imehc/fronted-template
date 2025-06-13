6d4e17b30d4c56c72baefc0cad08d9fa340eb43a
imehc <richohealthy@hotmail.com>
2025-06-13 18:21:27 +0800

    fix: startup error

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Icons from 'unplugin-icons/vite';
+import tailwindcss from '@tailwindcss/vite'
 import IconsResolver from 'unplugin-icons/resolver';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
@@ -40,6 +41,7 @@ export default defineConfig({
 			compiler: 'vue3',
 			autoInstall: true,
 		}),
+		 tailwindcss(),
 	],
 	server: {
 		port: 6012,
